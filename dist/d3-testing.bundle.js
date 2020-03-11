6ba49211e56ea0ac15d04344f86c4b84e73be75a
Richard Littauer <richard+github@burntfen.com>
2020-03-11 14:25:22 -0400

    Make chloropleth work with population

--- dist/d3-testing.bundle.js
+++ dist/d3-testing.bundle.js
@@ -101,11 +101,12 @@ const d3Geo = __webpack_require__(260)
 const topojson = __webpack_require__(524)
 
 // Set window height + width
-var width = 500
-var height = 650
+var width = 750
+var height = 1000
 
 // Define map projection
-var projection = d3Geo.geoTransverseMercator()
+var projection = d3Geo
+  .geoTransverseMercator()
   .rotate([72.57, -44.20])
   .translate([175, 185])
   .scale([12000])
@@ -120,15 +121,16 @@ var svg = d3.select('body').append('svg')
   .attr('height', height)
 
 // Define scale to sort data values into color buckets
-// var color = projection.scale.threshold()
-// .domain([10, 200, 1000, 2000, 5000, 10000, 20000, 40000, 50000])
-// .range(['#fff7ec', '#fee8c8', '#fdd49e', '#fdbb84', '#fc8d59', '#ef6548', '#d7301f', '#b30000', '#7f0000'])
+var color = d3
+  .scaleThreshold()
+  .domain([10, 200, 1000, 2000, 5000, 10000, 20000, 40000, 50000])
+  .range(['#fff7ec', '#fee8c8', '#fdd49e', '#fdbb84', '#fc8d59', '#ef6548', '#d7301f', '#b30000', '#7f0000'])
 
 // Legend Stuff
 
-// var y = d3.scaleSqrt()
-//     .domain([0, 50000])
-//     .range([0, 325])
+var y = d3.scaleSqrt()
+    .domain([0, 50000])
+    .range([0, 325])
 //
 // var yAxis = d3.svg.axis()
 //     .scale(y)
@@ -161,7 +163,7 @@ svg.append('path')
 var g = svg.append('g')
         .attr('class', 'key')
         .attr('transform', 'translate(320, 165)')
-        .call(yAxis)
+        // .call(yAxis)
 
 g.selectAll('rect')
         .data(color.range().map(function (d, i) {
@@ -179,59 +181,63 @@ g.selectAll('rect')
 
 svg.selectAll('.subunit')
         .data(topojson.feature(_vermont_json__WEBPACK_IMPORTED_MODULE_1__, _vermont_json__WEBPACK_IMPORTED_MODULE_1__.objects.vt_towns).features)
-    .enter().append('path')
+        .enter()
+        .append('path')
         .attr('d', path)
         .style('fill', function (d) {
           var population = d.properties.population
+          console.log(d.properties)
 
           if (population) {
+            console.log('test')
             return color(population)
           } else {
             return '#ddd'
           }
         })
 
-        .on('mouseover', function (d) {
-          var xPosition = d3.mouse(this)[0]
-          var yPosition = d3.mouse(this)[1] - 30
+        // .on('mouseover', function (d) {
+        //   var xPosition = d3.mouse(this)[0]
+        //   var yPosition = d3.mouse(this)[1] - 30
+        //
+        //   svg.append('text')
+        //         .attr('id', 'tooltip')
+        //         .attr('x', xPosition)
+        //         .attr('y', yPosition)
+        //         .attr('text-anchor', 'middle')
+        //         .attr('font-family', 'sans-serif')
+        //         .attr('font-size', '11px')
+        //         .attr('font-weight', 'bold')
+        //         .attr('fill', 'black')
+        //         .text(d.properties.town)
+        //
+        //   d3.select(this)
+        //     .style('fill', '#509e2f')
+        // })
+        // .on('mouseout', function (d) {
+        //   d3.select('#tooltip').remove()
+        //
+        //   d3.select(this)
+        //     .transition()
+        //     .duration(250)
+        //     .style('fill', function (d) {
+        //       var population = d.properties.population
+        //
+        //       if (population) {
+        //         return color(population)
+        //       } else {
+        //         return '#ddd'
+        //       }
+        //     })
+        // })
 
-          svg.append('text')
-                .attr('id', 'tooltip')
-                .attr('x', xPosition)
-                .attr('y', yPosition)
-                .attr('text-anchor', 'middle')
-                .attr('font-family', 'sans-serif')
-                .attr('font-size', '11px')
-                .attr('font-weight', 'bold')
-                .attr('fill', 'black')
-                .text(d.properties.town)
-
-          d3.select(this)
-            .style('fill', '#509e2f')
-        })
-        .on('mouseout', function (d) {
-          d3.select('#tooltip').remove()
-
-          d3.select(this)
-            .transition()
-            .duration(250)
-            .style('fill', function (d) {
-              var population = d.properties.population
-
-              if (population) {
-                return color(population)
-              } else {
-                return '#ddd'
-              }
-            })
-        })
-
-svg.append('path')
-        .datum(topojson.feature(_vermont_json__WEBPACK_IMPORTED_MODULE_1__, _vermont_json__WEBPACK_IMPORTED_MODULE_1__.objects.lake))
-        .attr('d', path)
-        .style('stroke', '#89b6ef')
-        .style('stroke-width', '1px')
-        .style('fill', '#b6d2f5')
+// Color lakes
+// svg.append('path')
+//         .datum(topojson.feature(vt, vt.objects.lake))
+//         .attr('d', path)
+//         .style('stroke', '#89b6ef')
+//         .style('stroke-width', '1px')
+//         .style('fill', '#b6d2f5')
 
 
 /***/ }),
